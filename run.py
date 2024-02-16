ab6982fbf8e4cc7a743a88af08c6106c998deeca
Jake Cunningham <jakecunningham1910@gmail.com>
2024-02-16 20:53:33 +0000

    Fixed input validation for player's coordinates and ensured computer's guesses are displayed on player's grid

--- run.py
+++ run.py
@@ -6,60 +6,52 @@ num_ships = 4
 # Initialize grids
 player_grid = [['.' for _ in range(grid_size)] for _ in range(grid_size)]
 computer_grid = [['.' for _ in range(grid_size)] for _ in range(grid_size)]
-revealed_player_grid = [['.' for _ in range(grid_size)] for _ in range(grid_size)]
-revealed_computer_grid = [['.' for _ in range(grid_size)] for _ in range(grid_size)]
 
 # Initialize scores
 player_score = 0
 computer_score = 0
 
 # Function to display a grid
-def display_grid(grid, hide_ships=False):
+def display_grid(grid, hide_ships=False, revealed_guesses=None):
     for i in range(grid_size):
         for j in range(grid_size):
-            if grid is player_grid and revealed_player_grid[i][j] == 'O':
-                print('O', end=' ')  # Display misses as 'O' for the player
-            elif grid is player_grid and revealed_player_grid[i][j] == '*':
-                print('*', end=' ')  # Display hits as '*' for the player
-            elif grid is computer_grid and revealed_computer_grid[i][j] == 'O':
-                print('O', end=' ')  # Display misses as 'O' for the computer
-            elif grid is computer_grid and revealed_computer_grid[i][j] == '*':
-                print('*', end=' ')  # Display hits as '*' for the computer
-            elif hide_ships and grid is computer_grid and revealed_computer_grid[i][j] == 'X':
-                print('.', end=' ')  # Print '.' to hide the ship for the computer
+            if hide_ships and grid is computer_grid:
+                if revealed_guesses and revealed_guesses[i][j] == '*':
+                    print('*', end=' ')  # Display hits as '*' for the computer's guesses
+                elif revealed_guesses and revealed_guesses[i][j] == 'O':
+                    print('O', end=' ')  # Display misses as 'O' for the computer's guesses
+                elif grid[i][j] == 'X':
+                    print('.', end=' ')  # Print '.' to hide the computer's ships
+                else:
+                    print(grid[i][j], end=' ')
             else:
                 print(grid[i][j], end=' ')
         print()
 
 # Function to randomly place ships on the grid
-def place_ships_randomly(grid, num_ships, hide_ships=False):
+def place_ships_randomly(grid, num_ships):
     for _ in range(num_ships):
         row = random.randint(0, grid_size - 1)
         col = random.randint(0, grid_size - 1)
-        if not (hide_ships and grid is computer_grid):
-            grid[row][col] = 'X'
+        grid[row][col] = 'X'
 
 # Function for the player's turn
 def player_turn():
-    global player_score
+    global computer_score
     while True:
         try:
             row = int(input("Enter the row to attack (0-4): "))
             col = int(input("Enter the column to attack (0-4): "))
             
             if 0 <= row < grid_size and 0 <= col < grid_size:
-                if revealed_computer_grid[row][col] != '.':
-                    print("You've already attacked this cell. Please choose another location.")
-                    continue
+                if computer_grid[row][col] == 'X':
+                    print("Hit!\n")
+                    computer_grid[row][col] = '*'
+                    computer_score += 1
                 else:
-                    if computer_grid[row][col] == 'X':
-                        print("Hit!\n")
-                        revealed_computer_grid[row][col] = '*'
-                        player_score += 1
-                    else:
-                        print("Miss!\n")
-                        revealed_computer_grid[row][col] = 'O'
-                    break
+                    print("Miss!\n")
+                    computer_grid[row][col] = 'O'
+                break
             else:
                 print("Coordinates must be within the range 0-4. Please try again.")
         except ValueError:
@@ -67,20 +59,19 @@ def player_turn():
 
 # Function for the computer's turn
 def computer_turn():
-    global computer_score
+    global player_score
     while True:
         row = random.randint(0, grid_size - 1)
         col = random.randint(0, grid_size - 1)
         
-        if revealed_player_grid[row][col] == '.':
-            if player_grid[row][col] == 'X':
-                print("Computer hit!\n")
-                revealed_player_grid[row][col] = '*'
-                computer_score += 1
-            else:
-                print("Computer missed!\n")
-                revealed_player_grid[row][col] = 'O'
-            break
+        if player_grid[row][col] == 'X':
+            print("Computer hit at ({}, {})!\n".format(row, col))
+            player_grid[row][col] = '*'
+            player_score += 1
+        else:
+            print("Computer missed at ({}, {})!\n".format(row, col))
+            player_grid[row][col] = 'O'
+        break
 
 # Function to play battleship game
 def play_battleship():
@@ -92,7 +83,7 @@ def play_battleship():
     
     # Randomly place ships for both player and computer
     place_ships_randomly(player_grid, num_ships)
-    place_ships_randomly(computer_grid, num_ships)  # Let the computer have ships
+    place_ships_randomly(computer_grid, num_ships)
     
     # Display player's grid
     print("Player's Grid:")
@@ -100,28 +91,28 @@ def play_battleship():
 
     # Display computer's grid
     print("Computer's Grid:")
-    display_grid(computer_grid)
-
+    display_grid(computer_grid, hide_ships=True)  # Hide computer's ships
+    
     # Main game loop
     while True:
         # Player's turn
         print("Player's Turn:")
-        player_turn()
+        computer_turn()
         print("Player's Grid:")
-        display_grid(player_grid)
+        display_grid(player_grid, revealed_guesses=computer_grid)
         print("Computer's Grid:")
-        display_grid(computer_grid)
+        display_grid(computer_grid, hide_ships=True)  # Hide computer's ships
         if player_score == 4:
             print("Congratulations! You win!")
             return False  # End the game
         
         # Computer's turn
         print("\nComputer's Turn:")
-        computer_turn()
+        player_turn()
         print("Player's Grid:")
-        display_grid(player_grid)
+        display_grid(player_grid, revealed_guesses=computer_grid)
         print("Computer's Grid:")
-        display_grid(computer_grid)
+        display_grid(computer_grid, hide_ships=True)  # Hide computer's ships
         if computer_score == 4:
             print("Computer wins! Better luck next time.")
             return False  # End the game
